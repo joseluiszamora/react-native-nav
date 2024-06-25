b65a00d6a775f4561e715445d34bbca4ee4e3716
jzamora <jzamora@miteleferico.bo>
2024-06-25 18:53:31 -0400

    navigate with buttons

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {useNavigation} from '@react-navigation/native';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalStyles.container}>
+      <PrimaryButton
+        onPress={() => navigation.navigate('Products' as never)}
+        label="Productos"
+      />
+
+      <PrimaryButton
+        onPress={() => navigation.navigate('Settings' as never)}
+        label="Settings"
+      />
     </View>
   );
 };
