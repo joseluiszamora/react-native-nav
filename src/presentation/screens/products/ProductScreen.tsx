aefb8d5c7c87804785443cdfe6ab403a0d9d12f5
jzamora <jzamora@miteleferico.bo>
2024-06-26 09:15:34 -0400

    navigator in screens

--- src/presentation/screens/products/ProductScreen.tsx
+++ src/presentation/screens/products/ProductScreen.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import {RootStackParams} from '../../routes/StackNavigator';
+import {globalStyles} from '../../theme/theme';
 
 export const ProductScreen = () => {
+  const params = useRoute<RouteProp<RootStackParams, 'Product'>>().params;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({title: params.name});
+  });
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
+    <View style={globalStyles.container}>
+      <Text>Product Screen</Text>
+
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: 'center',
+          marginTop: 20,
+        }}>
+        {params.id} - {params.name}
+      </Text>
     </View>
   );
 };
