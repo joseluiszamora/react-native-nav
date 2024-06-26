b19446ed7def879bc0370890b6a26e33335d1ff9
jzamora <jzamora@miteleferico.bo>
2024-06-26 17:26:50 -0400

    more changes

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Pressable, View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
 import {useNavigation, type NavigationProp} from '@react-navigation/native';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
 import {RootStackParams} from '../../routes/StackNavigator';
+import {Text} from 'react-native-reanimated/lib/typescript/Animated';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  // navigation.setOptions({
+  //   headerLeft: () => (
+  //     <Pressable>
+  //       <Text>Menu</Text>
+  //     </Pressable>
+  //   ),
+  // });
 
   return (
     <View style={globalStyles.container}>
